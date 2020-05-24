25ef70709a6f2bf88e06dd7754d57534fa204646
Depechie <glenn.versweyveld@gmail.com>
2020-05-25 00:42:02 +0200

    Added MD_Parola library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 // #include <SPI.h>
 
@@ -10,6 +11,7 @@
 #define CLK_PIN   D5  // or SCK
 #define DATA_PIN  D7  // or MOSI
 #define CS_PIN    D4  // or SS
+
 #define CHAR_SPACING  1 // pixels between characters
 #define BUF_SIZE  75
 
@@ -17,14 +19,17 @@ const char *ssid     = "Dep guests";
 const char *password;
 const long utcOffsetInSeconds = 7200;
 
+char message[BUF_SIZE];
+
 // char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
-MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+MD_Parola parolaClient = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
-void setup(){
-  mx.begin();
+void setup()
+{
+  parolaClient.begin();
 
   Serial.begin(115200);
 
@@ -38,79 +43,16 @@ void setup(){
   timeClient.begin();
 }
 
-void printText(uint8_t modStart, uint8_t modEnd, char *pMsg)
-// Print the text string to the LED matrix modules specified.
-// Message area is padded with blank columns after printing.
+void loop()
 {
-  uint8_t   state = 0;
-  uint8_t   curLen;
-  uint16_t  showLen;
-  uint8_t   cBuf[8];
-  int16_t   col = ((modEnd + 1) * COL_SIZE) - 1;
-
-  mx.control(modStart, modEnd, MD_MAX72XX::UPDATE, MD_MAX72XX::OFF);
-
-  do     // finite state machine to print the characters in the space available
-  {
-    switch(state)
-    {
-      case 0: // Load the next character from the font table
-        // if we reached end of message, reset the message pointer
-        if (*pMsg == '\0')
-        {
-          showLen = col - (modEnd * COL_SIZE);  // padding characters
-          state = 2;
-          break;
-        }
-
-        // retrieve the next character form the font file
-        showLen = mx.getChar(*pMsg++, sizeof(cBuf)/sizeof(cBuf[0]), cBuf);
-        curLen = 0;
-        state++;
-        // !! deliberately fall through to next state to start displaying
-
-      case 1: // display the next part of the character
-        mx.setColumn(col--, cBuf[curLen++]);
-
-        // done with font character, now display the space between chars
-        if (curLen == showLen)
-        {
-          showLen = CHAR_SPACING;
-          state = 2;
-        }
-        break;
-
-      case 2: // initialize state for displaying empty columns
-        curLen = 0;
-        state++;
-        // fall through
-
-      case 3:	// display inter-character spacing or end of message padding (blank columns)
-        mx.setColumn(col--, 0);
-        curLen++;
-        if (curLen == showLen)
-          state = 0;
-        break;
-
-      default:
-        col = -1;   // this definitely ends the do loop
-    }
-  } while (col >= (modStart * COL_SIZE));
-
-  mx.control(modStart, modEnd, MD_MAX72XX::UPDATE, MD_MAX72XX::ON);
-}
-
-void loop() {
   timeClient.update();
 
   // Serial.print(daysOfTheWeek[timeClient.getDay()]);
   
-  char message[BUF_SIZE];
-
-  // getCurrentTime().toCharArray(message, 75);
-  // sprintf (message, "%02u:%02u:%02u\n", timeClient.getHours(), timeClient.getMinutes(), timeClient.getSeconds());
-
   timeClient.getFormattedTime().toCharArray(message, 75);
-  printText(0, MAX_DEVICES-1, message);
+  
+  parolaClient.displayText(message, PA_LEFT, 0, 0, PA_PRINT, PA_NO_EFFECT);
+  parolaClient.displayAnimate();
+
   delay(1000);
 }
